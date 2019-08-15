7789891ef5b945a904fba650d686f99bcdccc914
Ryuta Eguchi <ryuta.eguchi@gmail.com>
2019-08-15 14:27:19 -0500

    Remove unused code

--- src/components/Common/MenuContent.js
+++ src/components/Common/MenuContent.js
@@ -1,8 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './MenuContent.css'
-
+import ProjectListContext from '../../contexts/ProjectListContext'
+import config from '../../config';
+import TokenService from '../../services/token-service';
 export default class MenuContent extends React.Component {
+    static contextType = ProjectListContext
+    constructor(props) {
+        super(props)
+        this.state = {
+            projectList: [],
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        fetch(`${config.API_ENDPOINT}/projects/`, {
+            method: 'GET',
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken()}`
+            }
+        })
+        .then(res =>
+            (!res.ok)
+              ? res.json().then(e => Promise.reject(e))
+              : res.json()
+        )
+        .then(resJson => {
+            console.log(resJson)
+            this.setState({
+                projectList: resJson
+            })
+        })  
+    }
+
+    renderProjectList() {
+        const { projectList } = this.state
+        return (
+            projectList.map(project => {
+                return (
+                    <Link to={`/project/${project.project_id}`}>
+                        <div>
+                            {project.name}
+                        </div>
+                    </Link>
+                )
+            })
+        )
+    }
+
     render() {
         return (
             <section className='menu-content'>
@@ -21,7 +67,9 @@ export default class MenuContent extends React.Component {
                         <li>Tasks I've completed</li>
                     </Link>
                     <li>My Projects</li>
+                    {this.renderProjectList()}
                 </ul>
+
             </section>
         )
     }
