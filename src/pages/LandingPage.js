05aadcd6fbbdc5de371d77c51778e89149a4c646
Ryuta Eguchi <ryuta.eguchi@gmail.com>
2019-09-05 11:20:30 -0500

    Modify landing page content and styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,17 +7,17 @@ export default class LandingPage extends React.Component {
     return (
       <div className='landing_page'>
         <div className='landing_page_text'>
+          <p className='landing_page_title'>ToDueBy</p>
           <p className='page_header'>Manage your team's projects and tasks</p>
-          <p className='sub_header'>
+          {/* <p className='sub_header'>
             <Link to='/register'>
               <span className='langing_page_links'>Register a user</span>
             </Link> and
             <Link to='/login'>
               <span className='langing_page_links'> login</span>
             </Link>
-          </p>
+          </p> */}
         </div>
-
       </div>
 
     )
